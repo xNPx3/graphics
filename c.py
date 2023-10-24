197c1206044c3d864ee07ca6c98c3559730606f0
xNPx3 <ninjapaimen@gmail.com>
2023-10-25 00:15:58 +0300

    cleaning

--- c.py
+++ c.py
@@ -2,6 +2,7 @@ import math
 import os
 import time
 import numpy as np
+import rotations as rot
 
 (WIDTH, HEIGHT) = os.get_terminal_size()
 
@@ -13,17 +14,19 @@ deg = (pi / 180)
 # light = ".,-~:;=!*#$@"
 light = "░▒▓█"
 
+draws = 0
+
 out = np.full((HEIGHT, WIDTH), ' ')
 zbfr = np.zeros((HEIGHT, WIDTH))
 points = np.mat([0, 0, 0])
 
 plot_scale = [1, 0.5]
-camera_pos = [0, 0]
+camera_pos = [0, 0, -10]
 
 np.seterr(divide='ignore', invalid='ignore')
 
 
-def clear():
+def clearBuffers():
     global points, out, zbfr
 
     #points = np.mat([0, 0, 0])
@@ -46,96 +49,91 @@ if out[row][col] != ' ':
 """
 
 
-def plot(data):
+def plotfunc(p, data):
     col = row = -1
 
     zvalues = data[:, 2]
     zmin = np.min(zvalues)
     zmax = np.max(zvalues)
-    # z_norm = (zvalues-np.min(zvalues))/(np.max(zvalues)-np.min(zvalues))
 
-    for p in data:
-        (x, y, z) = p.T
-        # center of screen, offset by scaled pos and camera pos
-        col = round(WIDTH / 2 + x.item() * plot_scale[0] - camera_pos[0] - 1)
-        row = round(HEIGHT / 2 - y.item() * plot_scale[1] + camera_pos[1] - 1)
+    (x, y, z) = p.T
+    # center of screen, offset by scaled pos and camera pos
+    col = round(WIDTH / 2 + x.item() * plot_scale[0] - camera_pos[0] - 1)
+    row = round(HEIGHT / 2 - y.item() * plot_scale[1] + camera_pos[1] - 1)
 
-        if (0 <= col < WIDTH) and (0 <= row < HEIGHT):
-            z_norm = ((-z - zmin) / (zmax - zmin)).item()
-            if z_norm > zbfr[row][col]:
-                # TODO: light source position
-                i = round(z_norm * (len(light) - 1))
-                char = light[i]
+    if (0 <= col < WIDTH) and (0 <= row < HEIGHT):
+        z_norm = ((-z - zmin) / (zmax - zmin)).item()
+        if z_norm >= zbfr[row][col]:
+            # TODO: light source position
+            i = round(z_norm * (len(light) - 1))
+            char = light[i]
 
-                out[row][col] = char
-                zbfr[row][col] = z_norm
+            out[row][col] = char
+            zbfr[row][col] = z_norm
 
 
-def draw():
+def plotfunc_perspective(p, data):
+    col = row = -1
+
+    trm = np.mat([
+        [1, 0, 0, 0],
+        [0, 1, 0, 0],
+        [0, 0, 1, 0],
+        [0, 0, 1, 0]
+    ])
+    p_4d = np.append(np.array(p), [1])
+    new_p = np.dot(trm, p_4d)
+
+    # TODO: perspective
+
+    zvalues = data[:, 2]
+    zmin = np.min(zvalues)
+    zmax = np.max(zvalues)
+
+    (x, y, z, w) = new_p.T
+    # center of screen, offset by scaled pos and camera pos
+    col = round(WIDTH / 2 + x.item() * plot_scale[0] - camera_pos[0] - 1)
+    row = round(HEIGHT / 2 - y.item() * plot_scale[1] + camera_pos[1] - 1)
+
+    if (0 <= col < WIDTH) and (0 <= row < HEIGHT):
+        z_norm = ((-z - zmin) / (zmax - zmin)).item()
+        if z_norm >= zbfr[row][col]:
+            # TODO: light source position
+            i = round(z_norm * (len(light) - 1))
+            char = light[i]
+
+            out[row][col] = char
+            zbfr[row][col] = z_norm
+
+
+def plot(data):
+    np.apply_along_axis(plotfunc, 1, data, data)
+
+
+def old_draw(clear=True):
     os.system('cls')
 
     for row in range(HEIGHT):
         for col in range(WIDTH):
             print(out[row][col], end='')
         print()
-    #print('\r', end='')
 
-    clear()
-
-
-# rotation matrices
-def RX(a):
-    return np.mat([
-        [1, 0, 0],
-        [0, cos(a), -sin(a)],
-        [0, sin(a), cos(a)]
-    ])
+    if clear:
+        clearBuffers()
 
 
-def RY(a):
-    return np.mat([
-        [cos(a), 0, sin(a)],
-        [0, 1, 0],
-        [-sin(a), 0, cos(a)]
-    ])
+def draw():
+    for row in range(HEIGHT):
+        for col in range(WIDTH):
+            print(out[row][col], end='')
+        print()
 
-
-def RZ(a):
-    return np.mat([
-        [cos(a), -sin(a), 0],
-        [sin(a), cos(a), 0],
-        [0, 0, 1]
-    ])
-
-
-def translate(x, y, z):
-    return np.mat([
-        [x, 0, 0],
-        [0, y, 0],
-        [0, 0, z]
-    ])
+    clearBuffers()
+    # cursor to home position (top left)
+    print(f'\033[H', end='')
 
 
 def hollowCube(s):
-    v = s // 2
-    for u in range(-v, v, 1):
-        addPoint(u, v, v)
-        addPoint(u, -v, v)
-        addPoint(u, v, -v)
-        addPoint(u, -v, -v)
-
-        addPoint(v, u, v)
-        addPoint(-v, u, v)
-        addPoint(v, u, -v)
-        addPoint(-v, u, -v)
-
-        addPoint(v, v, u)
-        addPoint(-v, v, u)
-        addPoint(v, -v, u)
-        addPoint(-v, -v, u)
-
-
-def hollowCube2(s):
     v = s // 2
     obj = np.array([[v, v, v]])
 
@@ -145,10 +143,12 @@ def hollowCube2(s):
             [u, -v, v],
             [u, v, -v],
             [u, -v, -v],
+
             [v, u, v],
             [-v, u, v],
             [v, u, -v],
             [-v, u, -v],
+
             [v, v, u],
             [-v, v, u],
             [v, -v, u],
@@ -160,41 +160,37 @@ def hollowCube2(s):
 
 def filledCube(s):
     w = s // 2
+    obj = np.array([[w, w, w]])
+
     for u in range(-w, w, 1):
         for v in range(-w, w, 1):
-            addPoint(u, v, w)
-            addPoint(u, w, v)
-            addPoint(w, u, v)
+            obj = np.concatenate((obj, np.mat([
+                [u, v, w],
+                [u, w, v],
+                [w, u, v],
 
-            addPoint(u, v, -w)
-            addPoint(u, -w, v)
-            addPoint(-w, u, v)
+                [u, v, -w],
+                [u, -w, v],
+                [-w, u, v],
+            ])))
+
+    return obj
 
 
 if __name__ == "__main__":
     print(WIDTH, HEIGHT)
+    os.system('cls')
 
-    hcube = hollowCube2(30)
-    hcube = hcube @ RY(60 * deg) @ RX(45 * deg)
+    c1 = hollowCube(40)
 
-    plot(hcube)
+    v = np.sqrt(3) / 3
+    rot1 = np.array([-v, -v, -v])
 
-    hcube2 = hollowCube2(10)
-    hcube2 = hcube2 @ RY(-20 * deg) @ RX(20 * deg)
-    plot(hcube2)
+    #c1 = c1 @ rot.Y(35 * deg) @ rot.X(30 * deg)
+    #plot(c1)
+    #draw()
 
-    draw()
-    time.sleep(2)
-
-    hcube2 = hcube2 @ translate(2, 1, 0)
-    plot(hcube2)
-    draw()
-
-
-"""
-    for i in range(2 * 360):
-        points = points @ RY(1 * deg) @ RZ(1 * deg)
-        plot(points)
+    for i in range(360):
+        c1 = c1 @ rot.Axis(1 * deg, rot1)
+        plot(c1)
         draw()
-        time.sleep(0.05)
-"""
---
+++ rotations.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+
+def X(a=0):
+    return np.mat([
+        [1, 0, 0],
+        [0, np.cos(a), -np.sin(a)],
+        [0, np.sin(a), np.cos(a)]
+    ])
+
+
+def Y(a=0):
+    return np.mat([
+        [np.cos(a), 0, np.sin(a)],
+        [0, 1, 0],
+        [-np.sin(a), 0, np.cos(a)]
+    ])
+
+
+def Z(a=0):
+    return np.mat([
+        [np.cos(a), -np.sin(a), 0],
+        [np.sin(a), np.cos(a), 0],
+        [0, 0, 1]
+    ])
+
+
+def Axis(a, unit):
+    (x, y, z) = unit.T
+    sin = np.sin(a)
+    cos = np.cos(a)
+    mc = (1 - cos)
+    return np.mat([
+        [cos + x * x * mc,      x * y * mc - z * sin,   x * z * mc + y * sin],
+        [x * y * mc + z * sin,  cos + y * y * mc,       y * z * mc - x * sin],
+        [x * z * mc - y * sin,  y * z * mc + x * sin,   cos + z * z * mc]
+    ])
