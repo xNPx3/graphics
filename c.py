b1c95186c1fa4d7620ef33b40d96ab0d79baea0d
xNPx3 <niittylasamuel@gmail.com>
2023-10-25 15:58:25 +0300

    fix

--- c.py
+++ c.py
@@ -2,7 +2,8 @@ import math
 import os
 import time
 import numpy as np
-import rotations as rot
+from numpy.linalg import multi_dot
+from rotations import X, Y, Z, Axis
 
 (WIDTH, HEIGHT) = os.get_terminal_size()
 
@@ -62,41 +63,8 @@ def plotfunc(p, data):
     row = round(HEIGHT / 2 - y.item() * plot_scale[1] + camera_pos[1] - 1)
 
     if (0 <= col < WIDTH) and (0 <= row < HEIGHT):
-        z_norm = ((-z - zmin) / (zmax - zmin)).item()
-        if z_norm >= zbfr[row][col]:
-            # TODO: light source position
-            i = round(z_norm * (len(light) - 1))
-            char = light[i]
-
-            out[row][col] = char
-            zbfr[row][col] = z_norm
-
-
-def plotfunc_perspective(p, data):
-    col = row = -1
-
-    trm = np.mat([
-        [1, 0, 0, 0],
-        [0, 1, 0, 0],
-        [0, 0, 1, 0],
-        [0, 0, 1, 0]
-    ])
-    p_4d = np.append(np.array(p), [1])
-    new_p = np.dot(trm, p_4d)
-
-    # TODO: perspective
-
-    zvalues = data[:, 2]
-    zmin = np.min(zvalues)
-    zmax = np.max(zvalues)
-
-    (x, y, z, w) = new_p.T
-    # center of screen, offset by scaled pos and camera pos
-    col = round(WIDTH / 2 + x.item() * plot_scale[0] - camera_pos[0] - 1)
-    row = round(HEIGHT / 2 - y.item() * plot_scale[1] + camera_pos[1] - 1)
-
-    if (0 <= col < WIDTH) and (0 <= row < HEIGHT):
-        z_norm = ((-z - zmin) / (zmax - zmin)).item()
+        z_norm = ((z - zmin) / (zmax - zmin)).item()
+        print(zmin, zmax, z_norm, end=' | ')
         if z_norm >= zbfr[row][col]:
             # TODO: light source position
             i = round(z_norm * (len(light) - 1))
@@ -107,6 +75,7 @@ def plotfunc_perspective(p, data):
 
 
 def plot(data):
+    #print(type(data), data)
     np.apply_along_axis(plotfunc, 1, data, data)
 
 
@@ -123,7 +92,8 @@ def old_draw(clear=True):
 
 
 def draw():
-    for row in range(HEIGHT):
+    print(' | debug')
+    for row in range(HEIGHT - 2):  # dont print last row to prevent jittering
         for col in range(WIDTH):
             print(out[row][col], end='')
         print()
@@ -177,20 +147,33 @@ def filledCube(s):
     return obj
 
 
+def translate(data, delta):
+    def _translate(p, d: np.array):
+        return np.sum([p, np.array(d)], axis=0)
+    return np.apply_along_axis(_translate, 1, data, delta)
+
+
 if __name__ == "__main__":
     print(WIDTH, HEIGHT)
     os.system('cls')
 
-    c1 = hollowCube(40)
+    c1 = hollowCube(30)
 
     v = np.sqrt(3) / 3
     rot1 = np.array([-v, -v, -v])
 
-    #c1 = c1 @ rot.Y(35 * deg) @ rot.X(30 * deg)
-    #plot(c1)
-    #draw()
+    c1 = multi_dot([c1, Y(35 * deg), X(20 * deg)])
+    c1 = translate(c1, [0, 0, 0])
+    # plot(c1)
 
-    for i in range(360):
-        c1 = c1 @ rot.Axis(1 * deg, rot1)
-        plot(c1)
+    #plot(np.mat([[10, 10, -1], [0, 0, -10]]))
+    # draw()
+
+    for i in range(301):
+        c = 2 * pi * (i / 100)
+        r = 20
+        x = np.cos(c) * r
+        y = np.sin(c) * r
+        plot(np.mat([[0, 0, 0], [x, y, 1], [-x, -y, 1]]))
         draw()
+        time.sleep(0.02)
