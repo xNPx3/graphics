f3be1e27b34d1018d2c22f557b9cc81a147356cc
xNPx3 <ninjapaimen@gmail.com>
2023-10-27 00:37:35 +0300

    time and fps optimizations

--- main.py
+++ main.py
@@ -34,6 +34,8 @@ def rad(a):  # degrees to radians
 def clearBuffers():  # clear frame buffers
     global FRAME_BUFFER, Z_BUFFER
 
+    # FRAME_BUFFER = np.where(FRAME_BUFFER != ' ', ' ', ' ')
+
     FRAME_BUFFER = np.full((HEIGHT, WIDTH), ' ')
     Z_BUFFER = np.zeros((HEIGHT, WIDTH))
 
@@ -55,37 +57,59 @@ def draw():  # draw frame buffer to terminal
     FRAMETIME = t
 
     # bottom row for debugging
-    print(f'{SHADE} | F:{str(CURRENT_FRAME).ljust(5)} | FPS:{str(FPS).ljust(5)} | {WIDTH}x{HEIGHT} | debug', end=' ')
+    print(f'{SHADE} | F:{CURRENT_FRAME} | FPS:{FPS} | {WIDTH}x{HEIGHT} | debug', end=' ')
+
+    # cursor to home position (top left)
+    print(f'\033[H', end='')
+
+
+def draw_optimized():  # draw frame buffer to terminal (optimized)
+    global CURRENT_FRAME, FRAMETIME
+
+    # dont print last row to prevent jittering
+    p = np.ravel(FRAME_BUFFER[:-1])
+    print(''.join(p), end='')
+
+    clearBuffers()
+
+    t = time.time()
+    FPS = round(1 / (t - FRAMETIME))
+    CURRENT_FRAME += 1
+    FRAMETIME = t
+
+    # bottom row for debugging
+    print(f'{SHADE} | F:{CURRENT_FRAME} | FPS:{FPS} | {WIDTH}x{HEIGHT} | debug', end=' ')
 
     # cursor to home position (top left)
     print(f'\033[H', end='')
 
 
 def plot(points):
-    def _plot(p, data):
+    def _plot(p, zmin, zmax, shading):
         col = row = -1
 
-        zvalues = data[:, 2]
-        zmin = np.min(zvalues)
-        zmax = np.max(zvalues)
-
         (x, y, z) = p.T
+
         # center of screen, offset by scaled pos and camera pos
-        col = round(WIDTH / 2 + x.item() * SCALING[0] - CAMERA_POS[0] - 1)
-        row = round(HEIGHT / 2 - y.item() * SCALING[1] + CAMERA_POS[1] - 1)
+        col = int(WIDTH / 2 + x * SCALING[0] - CAMERA_POS[0] - 1)
+        row = int(HEIGHT / 2 - y * SCALING[1] + CAMERA_POS[1] - 1)
 
         if (0 <= col < WIDTH) and (0 <= row < HEIGHT):
-            z_norm = ((z.item() - zmin) / (zmax - zmin))
+            z_norm = ((z - zmin) / (zmax - zmin))
             if z_norm >= Z_BUFFER[row][col]:
                 # TODO: light source position
-                i = round(z_norm * (len(SHADE) - 1))
-                char = SHADE[i]
+                i = round(z_norm * shading)
 
-                FRAME_BUFFER[row][col] = char
+                FRAME_BUFFER[row][col] = SHADE[i]
                 Z_BUFFER[row][col] = z_norm
 
+    zvalues = points[:, 2]
+    zmin = np.min(zvalues)
+    zmax = np.max(zvalues)
+    s = (len(SHADE) - 1)
+
     # apply function for every point
-    np.apply_along_axis(_plot, 1, points, points)
+    np.apply_along_axis(_plot, 1, points, zmin, zmax, s)
 
 
 def plotXYAxis():  # Draw the X and Y axises
@@ -108,18 +132,18 @@ def translate(data, delta):  # cheap point translation function
 
 
 def main():
-    c1 = cubeV(20)
+    c1 = cube(20)
     c1 = translate(c1, [30, 0, 0])
-    while CURRENT_FRAME <= 600:
+    while CURRENT_FRAME < 100:
         c1 = multi_dot([c1, Y(rad(1))])
         plotXYAxis()
         plot(c1)
-        draw()
+        draw_optimized()
 
 
 if __name__ == "__main__":
     os.system('cls')
-    print('\033[?25l')
+    print('\033[?25l', end='')
 
     main()
 
--- rotations.py
+++ rotations.py
@@ -2,7 +2,7 @@ import numpy as np
 
 # Rotate around X-axis
 def X(a=0):
-    return np.mat([
+    return np.array([
         [1, 0, 0],
         [0, np.cos(a), -np.sin(a)],
         [0, np.sin(a), np.cos(a)]
@@ -10,7 +10,7 @@ def X(a=0):
 
 # Rotate around Y-axis
 def Y(a=0):
-    return np.mat([
+    return np.array([
         [np.cos(a), 0, np.sin(a)],
         [0, 1, 0],
         [-np.sin(a), 0, np.cos(a)]
@@ -18,7 +18,7 @@ def Y(a=0):
 
 # Rotate around Z-axis
 def Z(a=0):
-    return np.mat([
+    return np.array([
         [np.cos(a), -np.sin(a), 0],
         [np.sin(a), np.cos(a), 0],
         [0, 0, 1]
@@ -30,7 +30,7 @@ def Axis(a, unit):
     sin = np.sin(a)
     cos = np.cos(a)
     mc = (1 - cos)
-    return np.mat([
+    return np.array([
         [cos + x * x * mc,      x * y * mc - z * sin,   x * z * mc + y * sin],
         [x * y * mc + z * sin,  cos + y * y * mc,       y * z * mc - x * sin],
         [x * z * mc - y * sin,  y * z * mc + x * sin,   cos + z * z * mc]
